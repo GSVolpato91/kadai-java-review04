0a9c09b292e0176e06742372669ec16bb202831c
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-29 21:17:59 +0900

    2nd review human class

--- human class assigment/src/animal/Human.java
+++ human class assigment/src/animal/Human.java
@@ -6,15 +6,27 @@ public class Human extends Animal implements Thinkable{
 
     private String hobby;
 
-    public Human (String getName, int getAge, String Hobby) {
-        getName();
-        getAge();
+    public String getHobby() {
+        return hobby;
+    }
+
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+
+    public Human (String name, int age, String hobby) {
+        super .setName(name);
+        super .setAge(age);
+        this.hobby = hobby;
 
     }
 
 
     @Override
     public void think() {
+
         System.out.println ("私は"+ hobby + "について考えています。");
 
     }
